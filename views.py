c9808d4f258ee191d0f294e4e0ca1158ff5917f1
andywar65 <andy.war1965@gmail.com>
2020-12-08 09:04:23 +0100

    added building alerts in context

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.views.generic.dates import YearArchiveView, DayArchiveView
 from django.utils.crypto import get_random_string
 from django.contrib.auth.mixins import PermissionRequiredMixin
@@ -17,13 +17,31 @@ class BuildingListView(PermissionRequiredMixin, ListView):
     context_object_name = 'builds'
     paginate_by = 12
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if 'created' in self.request.GET:
+            context['created'] = self.request.GET['created']
+        elif 'modified' in self.request.GET:
+            context['modified'] = self.request.GET['modified']
+
+        return context
+
+class BuildingDetailView(DetailView):
+    model = Building
+    context_object_name = 'build'
+    slug_field = 'slug'
+
 class BuildingCreateView( PermissionRequiredMixin, CreateView ):
     model = Building
-    permission_required = 'portfolio.add_building'
+    permission_required = 'bimblog.add_building'
     form_class = BuildingCreateForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        if 'created' in self.request.GET:
+            context['created'] = self.request.GET['created']
+        elif 'modified' in self.request.GET:
+            context['modified'] = self.request.GET['modified']
         #we add the following to feed the map
         context['mapbox_token'] = settings.MAPBOX_TOKEN
 
@@ -31,6 +49,22 @@ class BuildingCreateView( PermissionRequiredMixin, CreateView ):
 
     def get_success_url(self):
         if 'add_another' in self.request.POST:
-            return reverse('bimblog:building_create')
+            return (reverse('bimblog:building_create') +
+                f'?created={self.object.title}')
         else:
-            return reverse('bimblog:building_list')
+            return (reverse('bimblog:building_list') +
+                f'?created={self.object.title}')
+
+class BuildingUpdateView(PermissionRequiredMixin, UpdateView):
+    model = Building
+    permission_required = 'bimblog.change_building'
+    form_class = BuildingCreateForm
+    template_name = 'bimblog/building_update_form.html'
+
+    def get_success_url(self):
+        if 'add_another' in self.request.POST:
+            return (reverse('bimblog:building_create') +
+                f'?modified={self.object.title}')
+        else:
+            return (reverse('bimblog:building_list') +
+                f'?modified={self.object.title}')
