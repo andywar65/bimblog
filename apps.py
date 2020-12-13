a83165c3979e799f8040cc78c2f0ef8c1f9f4f87
andywar65 <andy.war1965@gmail.com>
2020-12-13 23:38:02 +0100

    added building plan and photostation permissions

--- apps.py
+++ apps.py
@@ -7,7 +7,11 @@ def create_bimblog_group(sender, **kwargs):
     grp, created = Group.objects.get_or_create(name=_('Building Manager'))
     if created:
         permissions = Permission.objects.filter(codename__in=('view_building',
-            'add_building', 'change_building', 'delete_building',))
+            'add_building', 'change_building', 'delete_building',
+            'view_buildingplan', 'add_buildingplan', 'change_buildingplan',
+            'delete_buildingplan',
+            'view_photostation', 'add_photostation', 'change_photostation',
+            'delete_photostation',))
         grp.permissions.set(permissions)
 
 class BimblogConfig(AppConfig):
