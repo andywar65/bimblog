1aad7a6642a0d002b1dea70bec324f4cefbc7a8d
andywar65 <andy.war1965@gmail.com>
2021-02-01 17:53:33 +0100

    deleting unused fields from models

--- models.py
+++ models.py
@@ -78,8 +78,6 @@ class Building(models.Model):
         help_text=_("Coordinates from Google Maps or https://openstreetmap.org"))
     zoom = models.FloatField(_("Zoom factor"), default = settings.CITY_ZOOM,
         help_text=_("Maximum should be 21"))
-    disciplines = models.ManyToManyField(Discipline,
-        blank = True, verbose_name = _('Show only plans belonging to:'), )
     disciplinesn = models.ManyToManyField(DisciplineNode,
         blank = True, verbose_name = _('Show only plans belonging to:'), )
 
@@ -112,9 +110,6 @@ class BuildingPlan(models.Model):
 
     build = models.ForeignKey(Building, on_delete = models.CASCADE,
         related_name='building_plan', verbose_name = _('Building'))
-    disc = models.ForeignKey(Discipline, on_delete = models.SET_NULL,
-        related_name='discipline_plan', verbose_name = _('Discipline'),
-        null=True, blank=True)
     discn = models.ForeignKey(DisciplineNode, on_delete = models.SET_NULL,
         related_name='disciplinen_plan', verbose_name = _('Discipline'),
         null=True, blank=True)
