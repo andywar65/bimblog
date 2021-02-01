5fa6edadf9b47547e4ed96c5ac170138e3300b41
andywar65 <andy.war1965@gmail.com>
2021-02-01 13:37:35 +0100

    added discipline node to forms

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
 from django.utils.translation import gettext as _
 
 from .models import (Building, BuildingPlan, PhotoStation, StationImage,
-    Discipline)
+    Discipline, DisciplineNode)
 
 class BuildingCreateForm(ModelForm):
     image = forms.ImageField(label=_('Image'), required=True)
@@ -11,14 +11,14 @@ class BuildingCreateForm(ModelForm):
     class Meta:
         model = Building
         fields = ( 'image', 'title', 'intro', 'date', 'address', 'lat', 'long',
-            'zoom', 'disciplines')
+            'zoom', 'disciplinesn')
 
 class BuildingUpdateForm(ModelForm):
 
     class Meta:
         model = Building
         fields = ( 'image', 'title', 'intro', 'date', 'address', 'lat', 'long',
-            'zoom', 'disciplines')
+            'zoom', 'disciplinesn')
 
 class BuildingDeleteForm(forms.Form):
     delete = forms.BooleanField( label=_("Delete the building"),
@@ -37,14 +37,14 @@ class PhotoStationCreateForm(ModelForm):
     build = forms.ModelChoiceField( label=_('Building'),
         queryset=Building.objects.all(), disabled = True )
 
-    class Meta:
-        model = PhotoStation
-        fields = '__all__'
-
     def __init__(self, **kwargs):
         super(PhotoStationCreateForm, self).__init__(**kwargs)
         #filter plan queryset
         self.fields['plan'].queryset = BuildingPlan.objects.filter(build_id=self.initial['build'])
+
+    class Meta:
+        model = PhotoStation
+        fields = '__all__'
 
 class StationImageCreateForm(ModelForm):
     stat = forms.ModelChoiceField( label=_('Photo station'),
@@ -67,4 +67,10 @@ class DisciplineCreateForm(ModelForm):
 
     class Meta:
         model = Discipline
+        fields = '__all__'
+
+class DisciplineNodeCreateForm(ModelForm):
+
+    class Meta:
+        model = DisciplineNode
         fields = '__all__'
